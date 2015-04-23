c89a558d829821d9424a779272f05a5c79728ec7
Chris Torstenson <c.torsday+github@gmail.com>
2015-04-23 16:06:25 -0700

    comment out all of js for now

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-function setBackgroundColor(color) {
-  document.body.style.backgroundColor = color
-}
-
-function setFontColor(color) {
-  document.body.style.color = color
-}
-
-setBackgroundColor('blue')
-
-dt = new Date()
-time_obj = dt.toString().split(" ")[4].split(":")[0] // get hour
-time_obj = Number(time_obj)
-console.log(time_obj)
-
-if (time_obj < 5 || time_obj > 21) { // night theme
-  setTheme('night')
-} else {
-  setTheme('day')
-}
-
-
-function setTheme(theme) {
-  if (theme == 'dark') {
-
-  } elsif (theme == 'day') {
-    console.log("TEST")
-    // setBackgroundColor('#846a7e')
-    // setFontColor('#191b88')
-  }
-}
+// function setBackgroundColor(color) {
+//   document.body.style.backgroundColor = color
+// }
+//
+// function setFontColor(color) {
+//   document.body.style.color = color
+// }
+//
+// setBackgroundColor('blue')
+//
+// dt = new Date()
+// time_obj = dt.toString().split(" ")[4].split(":")[0] // get hour
+// time_obj = Number(time_obj)
+// console.log(time_obj)
+//
+// if (time_obj < 5 || time_obj > 21) { // night theme
+//   setTheme('night')
+// } else {
+//   setTheme('day')
+// }
+//
+//
+// function setTheme(theme) {
+//   if (theme == 'dark') {
+//
+//   } elsif (theme == 'day') {
+//     console.log("TEST")
+//     // setBackgroundColor('#846a7e')
+//     // setFontColor('#191b88')
+//   }
+// }
